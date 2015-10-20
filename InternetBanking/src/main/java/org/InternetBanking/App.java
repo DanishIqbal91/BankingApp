9cc011238308f49a8b843fa89cb6d82b8986e47a
Mujtaba <techblogmaster@gmail.com>
2015-10-20 13:38:49 +0530

    Created new project

---
+++ InternetBanking/src/main/java/org/InternetBanking/App.java
@@ -0,0 +1,13 @@
+package org.InternetBanking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
